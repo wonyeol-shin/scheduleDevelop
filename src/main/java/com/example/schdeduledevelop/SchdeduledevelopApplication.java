9e8d57c18d49e40e8255f627844467a1fab10e62
wonyeolShin <lwy171400@gmail.com>
2026-07-07 10:32:37 +0900

    프로젝트 기본 구조

---
+++ src/main/java/com/example/schdeduledevelop/SchdeduledevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.schdeduledevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchdeduledevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchdeduledevelopApplication.class, args);
+    }
+
+}
